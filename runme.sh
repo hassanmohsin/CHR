7a722fb9fdd721209dfd0f5694c0f1819524805e
Md Mahmudulla Hassan <mhassan@miners.utep.edu>
2020-09-14 00:37:54 -0600

    add test command

--- runme.sh
+++ runme.sh
@@ -2,4 +2,8 @@
 # select gpu devices
 #export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-python -m chr.main -c configs/train_config.json |& tee -a log
+# train
+# python -m chr.main -c configs/train_config.json |& tee -a log
+
+# test
+python -m chr.main -c configs/test_config.json |& tee -a test.log
